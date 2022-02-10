b27d5c25ce24134b744c27b6307ff3fb543a8a5f
zenatureza <arthur.pinheiro357@gmail.com>
2022-02-09 21:21:29 -0300

    Testing deployment

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -25,7 +25,8 @@ public class WeatherForecastController : ControllerBase
         {
             Date = DateTime.Now.AddDays(index),
             TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            Summary = "ESTA FERA AI MEU"
+            // Summary = Summaries[Random.Shared.Next(Summaries.Length)]
         })
         .ToArray();
     }
